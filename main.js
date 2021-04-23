7d0623112f3cc545bbc46a3b50b5865130ef34d7
sarah <skg@neofilm.us>
2021-04-22 22:24:54 -0400

    Add code & logic for project button dialog.

--- main.js
+++ main.js
@@ -2,4 +2,22 @@ $(document).ready(function(){
     $("#projects").tabs();
     $("ul").sortable({axis:"x", containment:"#projects"});
     $("ol").sortable({axis:"y", containment:"#projects"});
+    $("#btnAddProject").button()
+    .click(function(){
+        $("#project-dialog").dialog({width:400, resizable:false, modal:true,
+            buttons:{
+                "Add new project":function(){
+                    var projectName = $("#new-project").val();
+                    $("<li><a href='#" + projectName + "'>" + projectName + "</a></li>")
+                    .appendTo("#main");
+                    $("#projects").tabs("refresh");
+                    $("#new-project").val("");
+                    $(this).dialog("close");
+                },
+                "Cancel":function(){
+                    $("#new-project").val("");
+                    $(this).dialog("close");
+                } 
+            }});
+    });
 });

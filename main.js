ea9d66f3622931c4a2fc17c19b937749c5633740
sarah <skg@neofilm.us>
2021-04-27 20:19:37 -0400

    JS functionality for Add Task dialog box.

--- main.js
+++ main.js
@@ -2,7 +2,21 @@ $(document).ready(function(){
     $("#projects").tabs();
     $("ul").sortable({axis:"x", containment:"#projects"});
     $("ol").sortable({axis:"y", containment:"#projects"});
-    $("#btnAddTask").button();
+    $("#btnAddTask").button()
+    .click(function(){
+        $("#task-dialog").dialog({width:400, resizable:false, modal:true,
+            buttons:{
+                "Add new task": function(){
+                    $("#new-task").val("");
+                    $(this).dialog("close");
+                },
+                "Cancel": function(){
+                    $("#new-task").val("");
+                    $(this).dialog("close");
+                }
+            }
+        });
+    });
     $("#btnAddProject").button()
     .click(function(){
         $("#project-dialog").dialog({width:400, resizable:false, modal:true,

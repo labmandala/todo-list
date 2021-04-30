7af1bce46e0fc36a6719970b5cd087626beb3f52
sarah <skg@neofilm.us>
2021-04-29 20:03:06 -0400

    Function to remove corresponding li task from list

--- main.js
+++ main.js
@@ -2,6 +2,11 @@ $(document).ready(function(){
     $("#projects").tabs();
     $("ul").sortable({axis:"x", containment:"#projects"});
     $("ol").sortable({axis:"y", containment:"#projects"});
+    $("input[type=checkbox]").click(function(){
+        $(this).closest("li").slideUp(function(){
+            $(this).remove();
+        });
+    });
     $("#btnAddTask").button()
     .click(function(){
         $("#task-dialog").dialog({width:400, resizable:false, modal:true,
